1d62a8df8d22b30731dab28ef1fbae13b116be4a
Sohee Yeo <95061372+soheeyeo@users.noreply.github.com>
2022-04-27 21:29:13 +0900

    Add files via upload

---
+++ DAY21/javascript/002.js
@@ -0,0 +1,6 @@
+document.getElementById("one").innerHTML = "hello <strong>world document</strong>";
+console.log("hello world console");
+window.alert("hello world alert");
+document.write("hello!!");
+© 2022 GitHub, Inc.
+Terms
